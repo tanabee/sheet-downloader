679746e96e9a94667fe61aa37c5d4a209ea46b1d
tanabee <tanabee1986@gmail.com>
2019-07-15 22:08:24 +0900

    Apply Material UI to List

--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,44 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 export default class List extends React.Component {
   render() {
-    const list = this.props.values.map((row) => {
-      const rowValues = row.join(', ');
-      return <li key={row[1]}>{rowValues}</li>;
+    let values = this.props.values.concat();
+
+    if (values.length === 0) return <></>;
+
+    // TODO: key に index 指定してる部分の修正
+    const headerValues = values.shift();
+    const header = headerValues.map((value, index) => {
+      return (
+        <TableCell key={index}>{value}</TableCell>
+      );
+    });
+
+    const list = values.map((row, index) => {
+      const rowValues = row.map((value, index) => {
+        return (
+          <TableCell key={index}>{value}</TableCell>
+        );
+      });
+      return <TableRow key={row}>{rowValues}</TableRow>;
     });
 
     return (
-      <ul className="list">{list}</ul>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {header}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {list}
+        </TableBody>
+      </Table>
     );
   }
 }
