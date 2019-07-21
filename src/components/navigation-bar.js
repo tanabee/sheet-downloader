5cdd2be32e1d7e8194115e8184d1cbd0655ed08c
tanabee <tanabee1986@gmail.com>
2019-07-21 13:24:26 +0900

    Added Search Bar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -2,15 +2,49 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import GoogleAuth from './google-auth.js'
 
 const useStyles = makeStyles(theme => ({
-  root: {
+  grow: {
     flexGrow: 1,
   },
-  title: {
-    flexGrow: 1,
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 300,
+    },
   },
 }));
 
@@ -24,6 +58,20 @@ export default function Navigation(props) {
           <Typography className={classes.title}>
             Slide2Json
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Type Spreadsheet URL"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'Search' }}
+            />
+          </div>
+          <div className={classes.grow} />
           <GoogleAuth onSignedIn={() => props.onSignedIn()} />
         </Toolbar>
       </AppBar>
