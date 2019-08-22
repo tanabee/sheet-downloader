dce598458faf5f758917e77a3db250b26d97431b
tanabee <tanabee1986@gmail.com>
2019-08-22 09:09:40 +0900

    Add multiple button

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import yaml from 'js-yaml';
 
 const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  }
 }));
 
 export default function DownLoadButton(props) {
 
   const classes = useStyles();
+  const options = ['DOWNLOAD AS JSON', 'DOWNLOAD AS YAML'];
+  const [open, setOpen] = React.useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const anchorRef = React.useRef(null);
+
+  function handleClick() {
+    const yml = yaml.dump({a: 1, b: '1', c: { d: options }});
+    console.log(yml);
+    alert(`You clicked ${options[selectedIndex]}`);
+  }
+
+  function handleMenuItemClick(event, index) {
+    setSelectedIndex(index);
+    setOpen(false);
+  }
+
+  function handleToggle() {
+    setOpen(prevOpen => !prevOpen);
+  }
+
+  function handleClose(event) {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  }
 
   const downloadJSON = () => {
     let values = props.values.concat();
@@ -35,15 +69,47 @@ export default function DownLoadButton(props) {
   };
 
   return (
-    <div align="center">
-      <Button
-        className={classes.button}
-        variant="outlined"
-        color="primary"
-        onClick={downloadJSON}
-      >
-        Download as JSON
-      </Button>
-    </div>
+    <Grid container>
+      <Grid item xs={12} align="center">
+        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+          <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+          <Button
+            color="primary"
+            size="small"
+            aria-owns={open ? 'menu-list-grow' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
+        <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper id="menu-list-grow">
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList>
+                    {options.map((option, index) => (
+                      <MenuItem
+                        key={option}
+                        selected={index === selectedIndex}
+                        onClick={event => handleMenuItemClick(event, index)}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Grid>
+    </Grid>
   );
 }
